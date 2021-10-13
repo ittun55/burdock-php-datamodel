2835d11fa82c38696e40e23ce2824dba446f1b95
Hiraku Saito <ittun55@gmail.com>
2021-10-13 20:51:58 +0900

    added bin/dumpJsonSchema

--- src/dumpJsonSchema.php
+++ src/dumpJsonSchema.php
@@ -1,26 +1,45 @@
-#!/usr/bin/php -q
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Burdock\Config\Config;
 use iamcal\SQLParser;
 
-// テーブル名の確認
-if (empty($argv[2])) die("table name must be specified.");
+$msg = "arg1: path to config file." . PHP_EOL;
+$msg.= "arg2: object path to DB setting." . PHP_EOL;
+$msg.= "arg3: table name to dump schema." . PHP_EOL;
 
-// config.json ロードとパスの確認
+// $argv[1] config.json ロードとパスの確認
+if (empty($argv[1])) {
+    die($msg);
+}
 $config_path = __DIR__ . '/../' . $argv[1];
-if (!file_exists($config_path)) die("config file not found.");
+if (!file_exists($config_path)) {
+    die("config file not found in ${config_path}.");
+}
 $config = Config::load($config_path);
 
-// PDO の生成
-$pdo = createPdo($config->getValue('db.default'));
+// $argv[2] DB定義オブジェクトまでの config パス
+if (empty($argv[2])) {
+    die($msg);
+}
+$obj_path = $argv[2];
+if (!$config->hasValue($obj_path)) {
+    die("The config doesn't contain DB setting values");
+}
 
-$def = getTableDefs($pdo, $argv[2]);
+// argv[3] テーブル名の確認
+if (empty($argv[3])) {
+    die($msg);
+}
+if (empty($argv[3])) die("table name must be specified.");
+
+// PDO の生成
+$pdo = createPdo($config->getValue($obj_path));
+
+$def = getTableDefs($pdo, $argv[3]);
 $def_json = json_encode($def, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 
 echo $def_json . PHP_EOL;
-
 
 function createPdo(array $setting): PDO
 {
