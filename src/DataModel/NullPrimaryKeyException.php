823bbbcd6adcca4994ad068df73a2e4fec35cced
Hiraku Saito <ittun55@gmail.com>
2022-11-26 21:47:41 +0900

    small fix

--- src/DataModel/NullPrimaryKeyException.php
+++ src/DataModel/NullPrimaryKeyException.php
@@ -6,7 +6,7 @@ namespace Burdock\DataModel;
 
 class NullPrimaryKeyException extends \Exception
 {
-    public function __construct($message, $code = 0, Exception $previous = null)
+    public function __construct($message, $code = 0, \Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
